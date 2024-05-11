990173b6cc05da3e40e38d06d928b1c21f193e3b
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-05-11 21:19:37 +0300

    Add enable depth.

--- OpenGL-CG-Project/Main.cpp
+++ OpenGL-CG-Project/Main.cpp
@@ -46,6 +46,7 @@ LRESULT WINAPI WndProc(HWND hwnd, UINT m, WPARAM wp, LPARAM lp)
 		h = HIWORD(lp);
 		glViewport(0, 0, w, h);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
 		glFlush();
 		SwapBuffers(hdc);
 		break;
